700c04a9bd538f404be13d8b7706a4781bd1ec59
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-09-24 04:45:51 +0200

    Update konobarica.js

--- public/konobarica.js
+++ public/konobarica.js
@@ -72,28 +72,7 @@ function decreaseFontSize() {
     var newSize = parseInt(currentSize) - 2; // Smanji veličinu fonta za 2px
     messageArea.style.fontSize = newSize + "px";
 }
-//ZA TUBE STRIM NA DUGME
- const toggleBtn = document.getElementById('tube');
-  const retryBtn = document.getElementById('retryBtn');
-  const iframe = document.getElementById('w2g');
 
-  toggleBtn.addEventListener('click', () => {
-    const visible = iframe.style.display === 'block';
-
-    iframe.style.display = visible ? 'none' : 'block';
-    retryBtn.style.display = visible ? 'none' : 'inline-block';
-  });
-// ZA DRUGU SOBU (Play2-Kam)
-const toggleBtnKam = document.getElementById('kam');
-const retryBtnKam = document.getElementById('retryBtnKam');
-const iframeKam = document.getElementById('iframekam');
-
-toggleBtnKam.addEventListener('click', () => {
-  const visible = iframeKam.style.display === 'block';
-
-  iframeKam.style.display = visible ? 'none' : 'block';
-  retryBtnKam.style.display = visible ? 'none' : 'inline-block';
-});
 //SLIKE ZA POZADINU
 document.getElementById("pozadina").addEventListener("click", function() {
     // Proveri da li vec postoji prompt
@@ -153,4 +132,5 @@ document.getElementById("pozadina").addEventListener("click", function() {
 socket.on("updateBackground", (url) => {
     document.body.style.backgroundImage = "url('" + url + "')";
 });
+
 
