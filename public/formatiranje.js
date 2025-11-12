0286bc89ab234c43bbcbdb94754c7af384ab90ed
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-11-12 10:44:30 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -114,9 +114,12 @@ socket.on('chatMessage', function(data) {
     }
 }
 
+let chatAvatar = avatars[data.nickname + '_chat'] || avatars[data.nickname];
+let avatarHTML = chatAvatar ? `<img src="${chatAvatar}" class="inline-avatar">` : '';
+
    // Dodavanje sadržaja poruke
-    newMessage.innerHTML = `<strong>${data.nickname}:</strong> ${text.replace(/\n/g, '<br>').replace(/ {2}/g, '&nbsp;&nbsp;')} <span style="font-size: 0.8em; color: gray;">(${data.time})</span>`;
-    messageArea.prepend(newMessage);
+newMessage.innerHTML = `<strong>${data.nickname}:</strong> ${text} <span style="font-size: 0.8em; color: gray;">(${data.time})</span> ${avatarHTML}`;
+ messageArea.prepend(newMessage);
     
       // Snimi poruku ako je aktivno snimanje
 if (window.snimanjeAktivno) {
