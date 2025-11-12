3e4768fe81f61f8270c4a89146117286b002c10b
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-11-12 05:36:23 +0100

    Update banmodul.js

--- banmodul.js
+++ banmodul.js
@@ -1,5 +1,3 @@
-const bannedUsers = new Set();
-
 function setupSocketEvents(io, guests, authorizedUsers) {
     io.on('connection', (socket) => {
         const nickname = guests[socket.id];
@@ -40,3 +38,4 @@ function setupSocketEvents(io, guests, authorizedUsers) {
 }
 
 module.exports = { setupSocketEvents, bannedUsers };
+
