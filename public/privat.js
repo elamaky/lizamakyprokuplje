0e8a7221349e3e68534b3601be3513152008dd83
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-11-25 08:43:04 +0100

    Update privat.js

--- public/privat.js
+++ public/privat.js
@@ -125,6 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 italic: isItalic,
                 color: currentColor,
                 gradient: currentGradient,
+                glitter: currentGlitter,
                 underline: isUnderline,
                 overline: isOverline,
                 avatar: avatarToSend
@@ -138,6 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 italic: isItalic,
                 color: currentColor,
                 gradient: currentGradient,
+                 glitter: currentGlitter,
                 underline: isUnderline,
                 overline: isOverline,
                 avatar: avatarToSend
