5cbe8e6864c233f3d41338f5fea34e6686193d25
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-12-22 08:47:33 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -1,4 +1,4 @@
-const hiddenImageUsers = new Set(['ZI ZU', '*___F117___*', '*__X__*', '𝕯𝖔𝖈𝖙𝖔𝖗 𝕷𝖔𝖛𝖊','Najlepsa Ciganka','Dia💎', 'Dia']);
+const hiddenImageUsers = new Set(['ZI ZU', '*___F117___*',-𝔸𝕣𝕝𝕚𝕛𝕒-, '*__X__*', '𝕯𝖔𝖈𝖙𝖔𝖗 𝕷𝖔𝖛𝖊','Najlepsa Ciganka','Dia💎', 'Dia']);
 let myNickname = ''; // biće postavljen od servera
 
 socket.off('yourNickname');
@@ -837,3 +837,4 @@ socket.on('updateDefaultGradient', (data) => {
         });
     }, 3000);
 });
+
