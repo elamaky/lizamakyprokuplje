ffdf163d8cf4e3885cd729ddb02c3620d998f035
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-12-23 18:39:11 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -45,21 +45,23 @@ socket.on('chatMessage', (msgData) => {
   const text = msgData.text.toLowerCase();
 
   const konobaricaReactions = {
-    'pauza': 'Arlijo nesrećo moja, ovo maltretiranje trpim samo zbog tebe jer te volim. Ja se ubih od posla, a gosti ne daju bakšiš nikako. Hoću povišicu od 500%',
-      'umorna': 'Da, bas sam mnogo umorna, sve me boli , X tako bi mi prijale tvoje zlatne ruke sada da mi masiraju noge ',
+    'pauza': 'Arlijo nesrećo moja...',
+    'umorna': 'Da, bas sam mnogo umorna...',
     'red bul': 'Evo stiže odmah <img src="emoji gif/redbul.webp" style="height:40px;">',
-    'viski': 'Evo stiže odmah viski za Lepu i ostale goste <img src="emoji gif/viski.png" style="height:40px;">',
-    'vodka': 'Dok vi pijete vodku djavo sedi u cose i smeje se <img src="emoji gif/vodka.webp" style="height:40px;">',
-     'pivo': 'Naravno, Pivo samo za vas <img src="emoji gif/pivo.avif" style="height:40px;">',
-     'caj': 'Caj za Dj Sandru <img src="emoji gif/caj.avif" style="height:40px;">',
-     'kisela': 'Mj kisela je dobra za tebe , stabilizovace ti se pritisak  <img src="emoji gif/kisela.avif" style="height:40px;">',
-     'jede': 'Gladni i siti slobodno se posluzite , na racun galaksije  <img src="emoji gif/jede.avif" style="height:40px;">',
-     'pije': 'Nemojte se stideti samo se posluzite  <img src="emoji gif/jede.avif" style="height:40px;">',
-    'kokta': 'Ima kokte dovoljno za sve  <img src="emoji gif/kokta.webp" style="height:40px;">'
+    'viski': 'Evo stiže odmah viski <img src="emoji gif/viski.png" style="height:40px;">',
+    'vodka': 'Dok vi pijete vodku...',
+    'pivo': 'Naravno, Pivo <img src="emoji gif/pivo.avif" style="height:40px;">',
+    'caj': 'Caj za Dj Sandru <img src="emoji gif/caj.avif" style="height:40px;">',
+    'kisela': 'Mj kisela je dobra za tebe...',
+    'jede': 'Gladni i siti slobodno se posluzite...',
+    'pije': 'Nemojte se stideti samo se posluzite...',
+    'kokta': 'Ima kokte dovoljno za sve...'
   };
 
   for (const key in konobaricaReactions) {
-    if (text.includes(key)) {
+    const regex = new RegExp(`\\b${key}\\b`, 'i');
+
+    if (regex.test(text)) {
       setTimeout(() => {
         io.emit('message', {
           username: '<span class="konobarica">Konobarica</span>',
@@ -71,6 +73,7 @@ socket.on('chatMessage', (msgData) => {
     }
   }
 });
+
     socket.on('moveChatContainer', (data) => {
     if (typeof data.x === 'number' && typeof data.y === 'number') {
       chatContainerState.x = data.x;
@@ -220,6 +223,7 @@ if (defaultGradient.value) {
   socket.on('disconnect', () => {});
     });
 };
+
 
 
 
