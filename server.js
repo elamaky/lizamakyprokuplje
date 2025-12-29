aacdd509a0fcfafb6b4c21f3ca1fb9665ce1e0d8
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-12-29 11:58:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava');
-const softGuestBan = require('./banmodul'); // relativna putanja do banmodul.js
+const { setupSocketEvents } = require('./banmodul'); // Uvoz funkcije iz banmodula
 const konobaricamodul = require('./konobaricamodul'); // Uvoz konobaricamodul.js
 const slikemodul = require('./slikemodul');
 const pingService = require('./ping');
@@ -63,8 +63,8 @@ const bannedUsers = new Set();
 // Skladištenje informacija o gostima
 const guests = {};
 const guestsData = {};
-softGuestBan(io, guests);
 const assignedNumbers = new Set(); // Set za generisane brojeve
+setupSocketEvents(io, guests, bannedUsers); // Dodavanje guests i bannedUsers u banmodul
 const userColors = {}; // Ovdje čuvamo boje korisnika
 const sviAvatari = {};
 const userGradients = {};
@@ -240,5 +240,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server je pokrenut na portu ${PORT}`);
 });
+
 
 
