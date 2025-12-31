759f0643df98b98725d6236ca094f70b10368e1a
elamaky <nemirnaela@gmail.com>
2025-12-31 01:46:06 +0100

    Update banmodul.js

--- banmodul.js
+++ banmodul.js
@@ -1,41 +1,36 @@
 const mongoose = require('mongoose');
 
-// Schema i model za soft ban
 const SoftGuestSchema = new mongoose.Schema({
-    guestId: { type: String, unique: true },
+    guestId: { type: String, unique: true }, // nickname
     banned: { type: Boolean, default: false }
 });
+
 const SoftGuest = mongoose.model('SoftGuest', SoftGuestSchema);
 
-// Ko sme da ban-uje
+// Lista autorizovanih korisnika koji mogu banovati
 const authorizedUsers = new Set([
     'Radio Galaksija','ZI ZU','*___F117___*','*__X__*',
     '𝕯𝖔𝖈𝖙𝖔𝖗 𝕷𝖔𝖛𝖊','-𝔸𝕣𝕝𝕚𝕛𝕒-',
     'Najlepsa Ciganka','Dia💎','Dia'
 ]);
 
-module.exports = function initBanModule(io, guests) {
+module.exports = function softGuestBan(io, guests) {
 
-    io.on('connection', socket => {
+    io.on('connection', (socket) => {
 
-        // Pošalji ban listu novom klijentu
-        SoftGuest.find({ banned: true }).then(bannedGuests => {
-            socket.emit('bannedList', bannedGuests.map(g => g.guestId));
-        });
-
-        // Registracija korisnika
         socket.on('registerGuestIdentity', async ({ guestId }) => {
             if (!guestId) return;
 
-            guests[socket.id] = guestId;
-
             let guest = await SoftGuest.findOne({ guestId });
-            if (!guest) guest = await SoftGuest.create({ guestId, banned: false });
+            if (!guest) {
+                guest = await SoftGuest.create({ guestId, banned: false });
+            }
 
-            if (guest.banned) socket.emit('userBanned', guestId);
+            if (guest.banned) {
+                socket.emit('userBanned', guestId);
+            }
         });
 
-        // Toggle soft ban
         socket.on('toggleSoftGuestBan', async ({ guestId }) => {
             const requesterName = guests[socket.id];
             if (!authorizedUsers.has(requesterName)) return;
